05a17220c73b38a98500afa16934256e49208080
Ahui <813174627@qq.com>
2019-09-19 17:47:48 +0800

    'EchartsMap.vue,async 处理异步'

--- src/views/EchartsMap.vue
+++ src/views/EchartsMap.vue
@@ -1,5 +1,8 @@
 <template>
-    <div id="chartsMap"></div>
+    <div>
+      <Spin size="large" fix v-if="spinShow"></Spin>
+      <div id="chartsMap"></div>
+    </div>
 </template>
 
 <script>
@@ -13,78 +16,87 @@ export default {
       roam: true,
       customData: [],
       RenderItems: [],
-      message: '123456'
+      heatData: [],
+      spinShow: false
     }
   },
   mounted () {
-    this.getCustomData()
-    this.getLine()
+    this.setLine()
   },
   methods: {
-    getLine () {
+    async setLine () {
+      this.spinShow = true
+      let lineData = await this.LineData()
+      let customData = await this.CustomData()
+      this.heatData = lineData
+      this.customData = customData.nameArr
+      this.RenderItems = customData.RenderItemArr
+      this.spinShow = false
       let myChart = this.$echarts.init(document.getElementById('chartsMap'))
-      this.$store.dispatch('GetHzHeat').then((response) => {
+      let option = {
+        animation: false,
+        bmap: {
+          center: this.center,
+          zoom: this.zoom,
+          roam: this.roam
+        },
+        visualMap: {
+          show: false,
+          top: 'top',
+          min: 0,
+          max: 5,
+          seriesIndex: 0,
+          calculable: true,
+          inRange: {
+            color: ['blue', 'blue', 'green', 'yellow', 'red']
+          }
+        },
+        tooltip: {
+          formatter: '{a} : {c}'
+        },
+        series: [{
+          type: 'heatmap',
+          coordinateSystem: 'bmap',
+          data: this.heatData,
+          pointSize: 5,
+          blurSize: 6
+        },
+        {
+          name: '杭州市',
+          type: 'custom',
+          coordinateSystem: 'bmap',
+          renderItem: this.renderItem,
+          itemStyle: {
+            opacity: 0.5,
+            color: '#1089ff',
+            borderWidth: 2,
+            borderColor: '#000000'
+          },
+          data: this.customData,
+          z: -10
+        }]
+      }
+      myChart.setOption(option, true)
+    },
+    LineData () {
+      return this.$store.dispatch('GetHzHeat').then((response) => {
         const points = [].concat.apply([], response.map((track) => {
           return track.map(function (seg) {
             return seg.coord.concat([1])
           })
         }))
-        let option = {
-          animation: false,
-          bmap: {
-            center: this.center,
-            zoom: this.zoom,
-            roam: this.roam
-          },
-          visualMap: {
-            show: false,
-            top: 'top',
-            min: 0,
-            max: 5,
-            seriesIndex: 0,
-            calculable: true,
-            inRange: {
-              color: ['blue', 'blue', 'green', 'yellow', 'red']
-            }
-          },
-          tooltip: {
-            formatter: '{a} : {c}'
-          },
-          series: [{
-            type: 'heatmap',
-            coordinateSystem: 'bmap',
-            data: points,
-            pointSize: 5,
-            blurSize: 6
-          },
-          {
-            name: '杭州市',
-            type: 'custom',
-            coordinateSystem: 'bmap',
-            renderItem: this.renderItem,
-            itemStyle: {
-              opacity: 0.5,
-              color: '#1089ff',
-              borderWidth: 2,
-              borderColor: '#000000'
-            },
-            data: this.customData,
-            z: -10
-          }]
-        }
-        myChart.setOption(option, true)
+        return points
       })
     },
-    getCustomData () {
-      this.$store.dispatch('GetHzCity').then((response) => {
+    CustomData () {
+      return this.$store.dispatch('GetHzCity').then((response) => {
         const nameArr = [].concat.apply([], response.features.map((names) => {
           return names.properties.name
         }))
         const RenderItemArr = [].concat.apply([], response.features.map((names) => {
           return names.geometry.coordinates[0]
         }))
-        this.customData = nameArr
-        this.RenderItems = RenderItemArr
+        return {nameArr, RenderItemArr}
       })
     },
     renderItem (params, api) {
